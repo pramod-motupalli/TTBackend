355e6bd4846172d33a3e2cd127e999fb87d66db0
Sai Venkata Sirisha Motupalli <motupallisnmsdurgapramod531@gmail.com>
2025-08-25 11:41:06 +0530

    firebase modified

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json"); // Download this from Firebase Console
+const serviceAccount = require("/etc/secrets/firebase-service-account.json"); // Download this from Firebase Console
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
